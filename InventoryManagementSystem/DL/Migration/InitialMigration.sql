1bcc22c908a8eebb9f84bda37040372c35e443cf
abhisikta-sigilo <abhisikta826@sigilotech.com>
2026-04-02 15:27:32 +0530

    feat: add initial database migration

---
+++ InventoryManagementSystem/DL/Migration/InitialMigration.sql
@@ -0,0 +1,83 @@
+USE InventoryManagementSystem;
+GO
+
+CREATE TABLE Customers (
+    CustomerId INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(150) NOT NULL,
+    Phone BIGINT,
+
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ModifiedDate DATETIME NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Products (
+    ProductId INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(150) NOT NULL,
+    Description VARCHAR(500),
+    Price DECIMAL(10,2) NOT NULL,
+
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ModifiedDate DATETIME NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Inventory (
+    InventoryId INT IDENTITY(1,1) PRIMARY KEY,
+    ProductId INT NOT NULL,
+    Quantity INT NOT NULL,
+
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ModifiedDate DATETIME NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0,
+
+    CONSTRAINT FK_Inventory_Product
+    FOREIGN KEY (ProductId) REFERENCES Products(ProductId)
+);
+
+CREATE TABLE Status (
+    StatusId INT IDENTITY(1,1) PRIMARY KEY,
+    Description VARCHAR(100) NOT NULL,
+
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ModifiedDate DATETIME NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0
+);
+
+
+CREATE TABLE Orders (
+    OrderId INT IDENTITY(1,1) PRIMARY KEY,
+    CustomerId INT NOT NULL,
+    TotalAmount DECIMAL(10,2) NOT NULL,
+    OrderStatusId INT NOT NULL,
+    OrderDate DATETIME NOT NULL,
+
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ModifiedDate DATETIME NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0,
+
+    CONSTRAINT FK_Orders_Customer
+    FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId),
+
+    CONSTRAINT FK_Orders_Status
+    FOREIGN KEY (OrderStatusId) REFERENCES Status(StatusId)
+);
+
+CREATE TABLE OrderItems (
+    OrderItemId INT IDENTITY(1,1) PRIMARY KEY,
+    OrderId INT NOT NULL,
+    ProductId INT NOT NULL,
+    Quantity INT NOT NULL,
+    TotalPrice DECIMAL(10,2) NOT NULL,
+
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ModifiedDate DATETIME NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0,
+
+    CONSTRAINT FK_OrderItems_Order
+    FOREIGN KEY (OrderId) REFERENCES Orders(OrderId),
+
+    CONSTRAINT FK_OrderItems_Product
+    FOREIGN KEY (ProductId) REFERENCES Products(ProductId)
+);
