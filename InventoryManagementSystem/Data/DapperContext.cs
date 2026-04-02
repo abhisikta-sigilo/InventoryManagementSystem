bea567a72920f32454e5e9bba4828aec9224d805
abhisikta-sigilo <abhisikta826@sigilotech.com>
2026-04-02 13:03:16 +0530

    initial commit

---
+++ InventoryManagementSystem/Data/DapperContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace InventoryManagementSystem.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _connectionString = _configuration.GetConnectionString("DefaultConnection")
+                                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
