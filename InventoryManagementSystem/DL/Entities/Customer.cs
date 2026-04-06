52b4a48d35b781e8df65ee7fe370a768d94e5bee
abhisikta-sigilo <abhisikta826@sigilotech.com>
2026-04-06 11:11:46 +0530

    refactor: introduce BaseEntity for audit fields and inherit in entities

--- InventoryManagementSystem/DL/Entities/Customer.cs
+++ InventoryManagementSystem/DL/Entities/Customer.cs
@@ -1,14 +1,14 @@
 ﻿namespace InventoryManagementSystem.DL.Entities
 {
-    public class Customer
+    public class Customer : BaseEntity
     {
         public long CustomerId { get; set; }
+
         public required string Name { get; set; }
+
         public required string Email { get; set; }
+
         public string? Phone { get; set; }
 
-        public DateTime CreatedDate { get; set; }
-        public DateTime? ModifiedDate { get; set; }
-        public bool IsDeleted { get; set; }
     }
 }
