897f4af176064ae5ba670b8b432b3097b2ee0309
Abhisikta <abhisikta826@sigilotech.com>
2026-04-17 11:40:41 +0530

    feat(order): add order and order item entities

---
+++ DL/Entities/OrderItemEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace DL.Entities
+{
+    public class OrderItemEntity : BaseEntity
+    {
+        public long OrderItemId { get; set; }
+
+        public long OrderId { get; set; }
+
+        public long ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
