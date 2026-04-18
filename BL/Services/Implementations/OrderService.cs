8a045bc423465b43040210e5bf074b42c7b1e5e1
Abhisikta <abhisikta826@sigilotech.com>
2026-04-18 11:15:45 +0530

    feat(order): implement order service with validations and total calculation
    - Validate product existence
    - Validate inventory availability
    - Calculate total price
    - Handle insufficient stock scenarios

---
+++ BL/Services/Implementations/OrderService.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using BL.Services.Abstractions;
+using DL.Entities;
+using DL.Repositories.Abstractions;
+using Shared.DTOs.Order;
+using Shared.Enums;
+
+namespace BL.Services.Implementations
+{
+    public class OrderService(
+        IOrderRepository orderRepository,
+        IProductRepository productRepository,
+        IInventoryRepository inventoryRepository,
+        IMapper mapper
+        ) :IOrderService
+    {
+        public async Task<OrderResponseDto> CreateOrder(OrderCreateRequestDto orderCreateRequestDto)
+        {
+            decimal totalAmount = 0;
+
+            List<OrderItemEntity> orderItemEntities = 
+                mapper.Map<List<OrderItemEntity>>(orderCreateRequestDto.Items);
+
+            foreach (OrderItemEntity item in orderItemEntities)
+            {
+                ProductEntity? productEntity = await productRepository.GetProductById(item.ProductId);
+
+                if (productEntity == null)
+                    throw new Exception($"Product {item.ProductId} not found");
+
+                InventoryEntity? inventory =
+                    await inventoryRepository.GetInventoryByProductId(item.ProductId);
+
+                if (inventory == null)
+                    throw new Exception($"Inventory not found for product {item.ProductId}");
+
+                if (inventory.Quantity < item.Quantity)
+                    throw new Exception($"Insufficient stock for product {item.ProductId}");
+
+                decimal itemTotal = productEntity.Price * item.Quantity;
+
+                item.TotalPrice = itemTotal;
+
+                totalAmount += itemTotal;
+            }
+
+            OrderEntity orderEntity = mapper.Map<OrderEntity>(orderCreateRequestDto);
+
+            orderEntity.TotalAmount = totalAmount;
+            orderEntity.OrderStatusId = (int)OrderStatus.Pending;
+            orderEntity.OrderDate = DateTime.UtcNow;
+
+            long orderId = await orderRepository.CreateOrderWithItems(
+                orderEntity,
+                orderItemEntities);
+
+            orderEntity.OrderId = orderId;
+
+            OrderResponseDto orderResponseDto =
+                mapper.Map<OrderResponseDto>(orderEntity);
+
+            orderResponseDto.Items =
+                mapper.Map<List<OrderItemResponseDto>>(orderItemEntities);
+
+            return orderResponseDto;
+        }
+    }
+}
