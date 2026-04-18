3806c441ca97aff31d0ce44a5c2345c6830ad79e
Abhisikta <abhisikta826@sigilotech.com>
2026-04-18 11:17:19 +0530

    feat(order): add automapper profiles and order status enum

---
+++ InventoryManagementSystem/Mappings/Order/Response/OrderResponseMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DL.Entities;
+using Shared.DTOs.Order;
+
+namespace API.Mappings.Order.Response
+{
+    public class OrderResponseMappingProfile : Profile
+    {
+        public OrderResponseMappingProfile()
+        {
+            CreateMap<OrderEntity, OrderResponseDto>();
+
+            CreateMap<OrderItemEntity, OrderItemResponseDto>();
+        }
+    }
+}
